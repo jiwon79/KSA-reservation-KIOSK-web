e76dea5bf34d700172fd3acf3a3975dacddbec8d
jiwon79 <19-079@ksa.hs.kr>
2021-10-07 09:26:51 +0900

    refactor: Split api function to new file

--- src/js/function.js
+++ src/js/function.js
@@ -213,26 +213,6 @@ function checkMatched(number, name) {
   return false;
 }
 
-async function fetchGaonnuriAuth(stu_number, pw) {
-  var url = 'https://gaonnuri.ksain.net/api/PAuth.php';
-  var data = new FormData();
-  let gaonnuri_id = 'ksa'+stu_number.slice(0,2)+stu_number.slice(3,6);
-  let gaonnuri_pw = CryptoJS.MD5(pw);
-  console.log(gaonnuri_id, gaonnuri_pw)
-  
-  data.append('id', gaonnuri_id);
-  data.append('pw', gaonnuri_pw);
-  
-  let response = await fetch(url, {
-    method: 'POST', // or 'PUT'
-    body: data,
-  })
-  let result = await response.json();
-  console.log(result);
-  return result;
-}
-
-
 // reservatoin button
 async function reservation() {
   var reserveForm = document.reserveForm;
